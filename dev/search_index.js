e82efeea2836f5c90a3d58628b37b29f46cf8897
Documenter.jl <documenter@juliadocs.github.io>
2022-01-12 20:01:09 +0000

    build based on e8d9371

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EntropicToy","category":"page"},{"location":"#EntropicToy","page":"Home","title":"EntropicToy","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EntropicToy.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EntropicToy]","category":"page"}]
+}
